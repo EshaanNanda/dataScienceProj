f85ffe6033a38d6ddf854bc1e2ed54f0bb387695
Eshaan Nanda <eshaannanda04@gmail.com>
2025-07-10 11:29:41 +0530

    Data Ingestion done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.datScience import logger
+from src.datScience.pipeline.dataIngestion import DataIngestionTrainingPipeline
 
 logger.info("Welcome to our cutsom logging dataScience")
+
+STAGE_NAME=" Data Ingestion stage"
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
